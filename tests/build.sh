5b8fe4353e04df358b86655951a9129f758e48ff
tmonoi <t.thingwell@gmail.com>
2025-06-29 15:32:27 +0900

    fix build sh

--- tests/build.sh
+++ tests/build.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 npm pack
 
 mkdir ./build 2>/dev/null || true
-mv -f papr*.tgz ./build/papr.tgz
-tar xf ./build/papr.tgz --directory=./build/
+mv -f mongo-standard-schema*.tgz ./build/mongo-standard-schema.tgz
+tar xf ./build/mongo-standard-schema.tgz --directory=./build/
 
 cd tests/esm/
 rm -f package-lock.json
