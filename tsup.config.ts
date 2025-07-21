99818d9f4f7d120aa21c398400d6bf9805fb8543
tmonoi <t.thingwell@gmail.com>
2025-07-21 17:24:38 +0900

    fix build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/adapters/index.ts'],
+  entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
   clean: true,
   splitting: false,
   sourcemap: true,
-  minify: false,
-  target: 'node18',
+  minify: true,
+  target: 'node22',
   external: ['mongodb', 'zod'],
   outDir: 'dist',
   treeshake: true,
