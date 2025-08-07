42ea79af4da3c76981d292ce81ea160239ddbd76
tmonoi <t.thingwell@gmail.com>
2025-08-07 16:44:42 +0900

    rm

--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-// Re-export all types
-export type { PaprFilter, PaprFilterConditions, PaprRootFilterOperators } from './filter.js';
-export type { PaprUpdateFilter, PaprMatchKeysAndValues, OnlyFieldsOfType } from './update.js';
-export type { PaprProjection } from './projection.js';
-export type { WithId, OptionalId, StrictOptionalId, NumericType, WithMongoId } from './utils.js';
-export type { StandardSchemaV1, InferStandardInput, InferStandardOutput } from './standard-schema.js';
-export { isStandardSchemaV1 } from './standard-schema.js';
